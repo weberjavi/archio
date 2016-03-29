70af85c4ddde486ba597c6d32766dddc1d26bd4e
weberjavi <weberjavi@gmail.com>
2016-03-29 19:47:31 +0200

    Added style to forms

--- app/assets/javascripts/projects-edit/edit-map.js
+++ app/assets/javascripts/projects-edit/edit-map.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-    var map = L.map('project-map-container').setView([51.505, -0.09], 3);
+    var map = L.map('project-map-container').setView([51.505, -0.09], 2);
 
     L.tileLayer('https://{s}.tiles.mapbox.com/v4/javiabia.148f6b63/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiamF2aWFiaWEiLCJhIjoiS1ZyQ3BQYyJ9.v8yJTbF879AQ_t6j5XafiQ', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
